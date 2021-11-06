2a7dc19492d86f9a9582900c5298a4c807e6e426
marekwino23 <marekw1996@gmail.com>
2021-11-06 14:15:13 +0100

    setup github Api and filter table with this data

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="header">
+  <h1>Git Hub App</h1>
+  <img class="github" src="./assets/github-icon-38980.png"/>
+  </div>
+  <div class="field">
+  <input type="text" placeholder="Filter By Name" v-model="word">
+  </div>
+<div>
+  <table>
+    <th>id</th>
+    <th>name</th>
+    <th>photo</th>
+  <tr v-for="contact in filterContacts" :key="contact.id">
+    <td>{{contact.id}}</td>
+    <td>{{contact.login}}</td> 
+    <td><img :src="contact.avatar_url"></td>
+  </tr>
+  </table>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function(){
+    return{
+      contacts: [],
+      type:"",
+      word:""
+    }
+},
+
+computed:{
+  filterContacts: function(){
+       return this.contacts.filter((contact)=>{
+        return contact.login.toLowerCase().match(this.word.toLowerCase())
+      })
   }
+},
+mounted(){
+  fetch('https://api.github.com/users', {
+  method: 'GET', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+.then(response => response.json())
+.then(data => {
+  this.contacts = data
+})
+.catch((error) => {
+  console.error('Error:', error);
+})}
 }
 </script>
 
@@ -21,6 +63,72 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+}
+
+.github{
+  width: 9%;
+  margin-left: 1%;
+}
+
+.header{
+  display: flex;
+  justify-content: center;
+}
+
+h1:hover{
+  color:blue
+}
+
+.github:hover{
+  transform: scale(1.2);
+}
+
+table {
+    font-family: Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 50%;
+    margin-left: 25%;
+    margin-top: 4%;
+}
+
+table td, table th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+table tr:nth-child(even){background-color: #f2f2f2;}
+
+table tr:hover {background-color: #ddd;}
+
+table th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: center;
+  font-weight: bold;
+  background-color: #2278db;
+  color: white;
+}
+
+table th:hover{
+  background-color: rgb(42, 156, 201);
+}
+
+img{
+  width: 10%;
+}
+
+input[type=text] {
+    width: 31%;
+    padding: 12px 20px;
+    margin: 31px 0px 0px 0px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+li{
+  list-style-type:none;
 }
 </style>
